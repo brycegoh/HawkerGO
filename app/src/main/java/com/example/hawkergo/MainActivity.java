170af3e995dbcc6ad99b7c8e2b2478d02b8b0ee9
brycegoh <brycegoh96@gmail.com>
2022-03-15 16:26:23 +0800

    connected to firebase

--- app/src/main/java/com/example/hawkergo/MainActivity.java
+++ app/src/main/java/com/example/hawkergo/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.hawkergo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
