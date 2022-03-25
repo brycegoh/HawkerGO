1b436538dd2a09a3d77dd213442a36a8a9d32878
Umang <umang_gupta@mymail.sutd.edu.sg>
2022-03-25 11:44:52 +0800

    login and register working completely

--- app/src/main/java/com/example/hawkergo/MainActivity.java
+++ app/src/main/java/com/example/hawkergo/MainActivity.java
@@ -1,59 +1,40 @@
 package com.example.hawkergo;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-
-import com.example.hawkergo.services.firebase.utils.FirebaseConstants;
-//import com.example.hawkergo.services.firebase.utils.FirebaseCollectionRef;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnLogOut;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnLogOut = findViewById(R.id.btnLogout);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogOut.setOnClickListener(view ->{
+            mAuth.signOut();
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        });
+
     }
 
-    public void getUserProfile(){
-
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-        if(user != null){
-
-            // Name, email address
-
-
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-
-
-            // Check if user's email is verified
-
-            boolean emailVerified = user.isEmailVerified();
-
-            // The user's ID, unique to the Firebase project. Do NOT use this value to
-            // authenticate with your backend server, if you have one. Use
-            // FirebaseUser.getIdToken() instead.
-
-            String uid = user.getUid();
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null){
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
         }
     }
-
-
 }
