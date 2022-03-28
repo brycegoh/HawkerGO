0c998366b00c795ce12bce68000ded7d9df0481f
brycegoh <brycegoh96@gmail.com>
2022-03-28 10:54:51 +0800

    sync

--- app/src/main/java/com/example/hawkergo/MainActivity.java
+++ app/src/main/java/com/example/hawkergo/MainActivity.java
@@ -1,18 +1,9 @@
 package com.example.hawkergo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
-import com.example.hawkergo.activities.AddHawkerStall;
-import com.example.hawkergo.models.HawkerCentre;
-import com.example.hawkergo.models.Tags;
-import com.example.hawkergo.services.firebase.interfaces.DbEventHandler;
-import com.example.hawkergo.services.firebase.repositories.HawkerCentresRepository;
-import com.example.hawkergo.services.firebase.repositories.TagsRepository;
-
-import java.util.List;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,9 +16,5 @@ public class MainActivity extends AppCompatActivity {
         Intent toHawkerCentreIntent = new Intent(MainActivity.this, HawkerCentreActivity.class);
         startActivity(toHawkerCentreIntent);
         setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, AddHawkerStall.class);
-        intent.putExtra("id", "8Esh2FzcoCwxNJPmjdYB");
-        startActivity(intent);
     }
 }
