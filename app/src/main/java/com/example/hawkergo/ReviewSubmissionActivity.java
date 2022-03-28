94dda39f339ee4376b56939e3d59be39299f6fb2
ZhiZhangT <zztee24@gmail.com>
2022-03-28 23:06:05 +0800

    added in addReview function in ReviewSubmissionActivity, not tested yet

--- app/src/main/java/com/example/hawkergo/ReviewSubmissionActivity.java
+++ app/src/main/java/com/example/hawkergo/ReviewSubmissionActivity.java
@@ -1,7 +1,10 @@
 package com.example.hawkergo;
 
-import android.media.Rating;
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,21 +15,48 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.hawkergo.models.Review;
+import com.example.hawkergo.services.firebase.interfaces.DbEventHandler;
+import com.example.hawkergo.services.firebase.repositories.ReviewRepository;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Date;
+
 public class ReviewSubmissionActivity extends AppCompatActivity {
+
+    Intent intent = getIntent();
+    /*TODO get hawkerStallID from previous activity: HawkerStallActivity
+    String hawkerStallID = intent.getStringExtra(HawkerStallActivity.KEY, null);
+
+     */
+
     private RatingBar ratingBar;
     private EditText editText;
     private Button submitButton;
-    public float reviewStars;
+    public double reviewStars;
     public String reviewContent;
+
+    private FirebaseAuth mAuth;
+    String userDisplayName;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.review_submission);
 
         ratingBar = (RatingBar) findViewById(R.id.reviewRatingBar);
         editText = (EditText) findViewById(R.id.edit_review) ;
         submitButton = (Button) findViewById(R.id.review_submit_btn);
+
+        // get user's name <String>
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            userDisplayName = user.getDisplayName();
+        }
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +64,27 @@ public class ReviewSubmissionActivity extends AppCompatActivity {
                 reviewStars = ratingBar.getRating();
                 reviewContent = editText.getText().toString();
                 Toast.makeText(ReviewSubmissionActivity.this, R.string.reviewSubmitted, Toast.LENGTH_LONG).show();
-                // HawkerStallsRepository.addReview(user_name, reviewStars, reviewContent)
+
+                // get date reviewed = current date <Date>
+                Date currentDate = new Date();
+
+                /*TODO
+                Review review = new Review(userDisplayName, reviewContent, reviewStars, currentDate, hawkerStallID);
+
+
+                ReviewRepository.addReview(hawkerStallID, review, new DbEventHandler<String>() {
+                    @Override
+                    public void onSuccess(String o) {
+                        Log.i(TAG, "Successfully added review into Firestore for: "+ hawkerStallID);
+                    }
+
+                    @Override
+                    public void onFailure(Exception e) {
+                        Log.e(TAG, "Failed to add review into Firestore for: " + hawkerStallID);
+                    }
+                });
+
+                 */
             }
         });
     }
