fd9b5b8aea9622a5d033754aff8d0c32a1877fc1
brycegoh <brycegoh96@gmail.com>
2022-03-31 11:15:06 +0800

    added reviewcount

--- app/src/main/java/com/example/hawkergo/models/HawkerStall.java
+++ app/src/main/java/com/example/hawkergo/models/HawkerStall.java
@@ -8,11 +8,55 @@ public class HawkerStall extends BaseDbFields {
     // TODO: Need to add in imageUrl for the hawker stall
     // TODO: Do we want a field to track numReviews only? Thinking of storage space concerns
     private String address , name, imageUrl, hawkerCentreID;
+    private Integer reviewCount;
     private OpeningHours openingHours;
     private List<Review> reviews;
     private List<String> reviewsIds;
     private List<String> popularItems;
     private List<String> tags;
+
+    public HawkerStall(){};
+
+    public HawkerStall(String address, String name, HashMap<String,String> openingHours, String hawkerCentre, String imageUrl, List<String> reviewsIds){
+        this.address = address;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.reviewsIds = reviewsIds;
+    }
+
+    public HawkerStall(String address, String name, OpeningHours openingHours, String imageUrl, List<String> popularItems, List<String> tags, String hawkerCentreID){
+
+        this.address = address;
+        this.name = name;
+        this.imageUrl =imageUrl;
+        this.openingHours = openingHours;
+        this.imageUrl = imageUrl;
+        this.popularItems = popularItems;
+        this.tags = tags;
+        this.hawkerCentreID = hawkerCentreID;
+    }
+
+    public void attachReviews(List<Review> reviews){
+        this.reviews = reviews;
+    }
+
+
+    public HashMap<String, Object> toMap(){
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("address", this.address);
+        map.put("openingHours", this.openingHours);
+        map.put("name", this.name);
+        map.put("reviews", this.reviews);
+        return map;
+    }
+
+    public Integer getReviewCount() {
+        return reviewCount;
+    }
+
+    public void setReviewCount(Integer reviewCount) {
+        this.reviewCount = reviewCount;
+    }
 
     public String getAddress() {
         return address;
@@ -84,40 +128,5 @@ public class HawkerStall extends BaseDbFields {
 
     public void setAddress(String address) {
         this.address = address;
-    }
-
-    public HawkerStall(){};
-
-    public HawkerStall(String address, String name, HashMap<String,String> openingHours, String hawkerCentre, String imageUrl, List<String> reviewsIds){
-        this.address = address;
-        this.name = name;
-        this.imageUrl = imageUrl;
-        this.reviewsIds = reviewsIds;
-    }
-
-    public HawkerStall(String address, String name, OpeningHours openingHours, String imageUrl, List<String> popularItems, List<String> tags, String hawkerCentreID){
-
-        this.address = address;
-        this.name = name;
-        this.imageUrl =imageUrl;
-        this.openingHours = openingHours;
-        this.imageUrl = imageUrl;
-        this.popularItems = popularItems;
-        this.tags = tags;
-        this.hawkerCentreID = hawkerCentreID;
-    }
-
-    public void attachReviews(List<Review> reviews){
-        this.reviews = reviews;
-    }
-
-
-    public HashMap<String, Object> toMap(){
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("address", this.address);
-        map.put("openingHours", this.openingHours);
-        map.put("name", this.name);
-        map.put("reviews", this.reviews);
-        return map;
     }
 }
